a928ea11c977c46726ae1b8ad2335bd9f7c2f51e
dzmitry <zakharenka.dzmitry.1981@gmail.com>
2022-08-14 18:38:05 +0300

    stage0-module3-task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double result = toBeDivided;
+        result = (result/5);
+        System.out.println(result);
+
     }
 }
+
--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+
+        int first = 1;
+        String numFirst = Integer.toString(first);
+        int second = 10;
+        String numSecond = Integer.toString(second);
+        int third = 100;
+        String numThird = Integer.toString(third);
+        System.out.println(numFirst+numSecond+numThird);
     }
 }
+//Create 3 int variables with names: first, second, third. Initialize them with values = 1, 10, 100. Print all of them to console within one System.out.println statement (with no spaces between).
